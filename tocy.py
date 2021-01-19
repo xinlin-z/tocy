6a18ed80e120c60f9579333c52ee7232becd9fd2
xinlin <xinlin.zhang@hotmail.com>
2021-01-19 11:49:12 +0800

    fixup

--- tocy.py
+++ tocy.py
@@ -2,7 +2,6 @@ import sys
 import re
 
 
-# sys.argv[1] is *.md
 with open(sys.argv[1]) as f:
     lines = f.readlines()
 
@@ -18,15 +17,14 @@ for line in lines:
     if skip: continue
     # search and print out #+ title
     if strs:=re.match(r'\s*(#+)(.*)',line):
-        if (htl:=len((ht:=strs.groups()[0]))) > 6:  # max head level
+        if (htl:=len((ht:=strs.groups()[0]))) > 6:  # max head level is 6
             continue
         rest = strs.groups()[1].strip()
         # git rid of markdown syntax elements ~*_
         while a:=re.search(r'([~*_]{1,2})(.*)\1',rest):
             rest = rest[:a.start()] + a.groups()[1] + rest[a.end():]
-        # space to - and squeeze unsupported chars
-        rest = re.sub(r'\s', '-', rest)
-        rest = re.sub(r'[^-_0-9a-zA-Z]', '', rest)
+        rest = re.sub(r'\s', '-', rest)  # space --> -
+        rest = re.sub(r'[^-_0-9a-zA-Z]', '', rest)  # squeeze other chars
         print(''.join((' '*(htl-1)*4, '* ',
                        '[',strs.groups()[1].strip(),'](#', rest,')')))
 
